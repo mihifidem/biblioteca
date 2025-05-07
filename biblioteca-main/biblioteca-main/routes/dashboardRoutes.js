88136615c8f4bd01ec2e1e6b52e7c11e90e522eb
Oscar Burgos <44227726+mihifidem@users.noreply.github.com>
2025-05-07 10:53:18 +0200

    holi

--- biblioteca-main/biblioteca-main/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-
-// Listar usuarios
-router.get('/', dashboardController.menuDashboard);
-
-module.exports = router;
